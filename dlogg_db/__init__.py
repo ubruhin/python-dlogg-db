8693f712b647bef025ccc0036442ff73b99c821d
U. Bruhin <urbibruhin@bluewin.ch>
2017-04-09 15:00:49 +0200

    Fix missing import statement

--- dlogg_db/__init__.py
+++ dlogg_db/__init__.py
@@ -18,3 +18,4 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from upload import DLoggDbUpload
+from download import DLoggDbDownload
